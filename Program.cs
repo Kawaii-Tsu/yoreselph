10f78b736726a9329458cc9a1bfe565af4634ced
Kawaii-Tsu <LouisP3@learn.cssd.ab.ca>
2017-10-10 13:38:51 -0600

    added more stuff

--- Program.cs
+++ Program.cs
@@ -6,8 +6,17 @@ namespace yoreselph
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.Write("Haii");
+            Console.WriteLine("Type up 2 numbers");
+            float num1 = float.Parse(Console.ReadLine());
+            float num2 = float.Parse(Console.ReadLine());
+            Console.WriteLine(AddTwoNumbers(num1, num2)); 
+        }
+
+        public static float AddTwoNumbers(float n1, float n2)
+        {
+            float result = n1 + n2;
+
+            return result;
         }
     }
 }
