6c8ad1beb5700cb9754b74e778ac648958023a2e
Kawaii-Tsu <LouisP3@learn.cssd.ab.ca>
2017-10-10 13:11:24 -0600

    updated program

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace yoreselph
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.Write("Haii");
         }
     }
 }
